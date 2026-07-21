bafd511cb85122e67fa71db814e923a939f05c85
jocieljesus <jocielalvesdejesus@gmail.com>
2026-07-21 17:49:39 -0300

    Criando logica de validacao de email e nova tela de home

--- src/main/java/com/jociel/estoque/App.java
+++ src/main/java/com/jociel/estoque/App.java
@@ -2,8 +2,8 @@ package com.jociel.estoque;
 
 import javafx.application.Application;
 
-public class Launcher {
+public class App {
     public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
+        Application.launch(LoginApplication.class, args);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.jociel.estoque {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.jociel.estoque to javafx.fxml;
